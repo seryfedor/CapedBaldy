b7e1d583b9ae4dbd6fb98e5163d0b68a166dd499
John Doe <johndoe@example.com>
2021-03-19 22:50:37 +0300

    feat: send message to telegram bot

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ヒーローの帰還' });
+  res.render('index', { title: 'ヒーローの帰還', alert: req.query.msg });
 });
+
+router.post('/', (req,res) => {
+  // res.send('Post method...')
+
+  console.log(req.body.message)
+
+  var getUri = require('get-uri');
+  getUri('https://api.telegram.org/bot' + process.env.TELEGRAM_TOKEN + '/sendMessage?chat_id=' + process.env.TELEGRAM_CHAT_ID + '&text=' + req.body.message)
+
+  res.redirect(301,'/?msg=ok')
+})
 
 module.exports = router;
